1069155f0059b3ebd524a8709e4f2a8c51ca314c
byungchan <icoo08216@gmail.com>
2022-05-18 23:31:50 +0900

    Google 로그인 연동

---
+++ src/main/java/com/bc/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.bc.book.springboot.config.auth;
+
+import com.bc.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security 설정들 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점.
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated() // 설정된 URL 이외의 나머지 모든 URL 은 인증된 사용자들에게만 허용하도록
+                .and()
+                    .logout() // 로그아웃 기능에 대한 여러 설정의 진입점
+                        .logoutSuccessUrl("/")// 로그아웃 성공시 "/" 주소 이동.
+                .and()
+                    .oauth2Login() // OAuth2 로그인 기능에 대한 설정 진입점
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService); // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+    }
+}
--- src/main/java/com/bc/book/springboot/web/IndexController.java
+++ src/main/java/com/bc/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bc.book.springboot.web;
 
+import com.bc.book.springboot.config.auth.dto.SessionUser;
 import com.bc.book.springboot.service.PostsService;
 import com.bc.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDESC());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,4 +41,6 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 }
