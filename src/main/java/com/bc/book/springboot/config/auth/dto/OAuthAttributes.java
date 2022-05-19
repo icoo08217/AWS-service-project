a32f5b478d403432a6c0f20c30f93768b9b715ac
byungchan <icoo08216@gmail.com>
2022-05-19 15:46:51 +0900

    Naver 로그인 연동

--- src/main/java/com/bc/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/bc/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,13 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId , String userNameAttributeName, Map<String , Object> attributes) {
+    public static OAuthAttributes of(String registrationId ,
+                                     String userNameAttributeName,
+                                     Map<String , Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -35,6 +41,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNAmeAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNAmeAttributeName , Map<String , Object> attributes){
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNAmeAttributeName)
                 .build();
     }
--- src/main/java/com/bc/book/springboot/web/IndexController.java
+++ src/main/java/com/bc/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bc.book.springboot.web;
 
+import com.bc.book.springboot.config.auth.LoginUser;
 import com.bc.book.springboot.config.auth.dto.SessionUser;
 import com.bc.book.springboot.service.PostsService;
 import com.bc.book.springboot.web.dto.PostsResponseDto;
@@ -10,18 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model , @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDESC());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
