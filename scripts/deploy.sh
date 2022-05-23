ab3800d4694057c7cec7efc76d53968392dcf3bb
byungchan <icoo08216@gmail.com>
2022-05-23 23:34:01 +0900

    오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,6 +32,6 @@ echo "> JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
+# 오타 조심
 nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
 -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
